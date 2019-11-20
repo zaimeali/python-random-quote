61946503dffb3206f05983a3aa30316834a891a3
Zaime Ali <zaimeali1997@gmail.com>
2019-11-20 13:47:53 +0500

    Final Output

--- get-quote.py
+++ get-quote.py
@@ -10,7 +10,15 @@ def primary():
   last = len(quotes) - 1
   rnd = random.randint(0, last)
 
+  print(quotes[rnd], end = ' ')
+
+  rnd = random.randint(0, last)
   print(quotes[rnd])
+
+  f = open("quotes.txt", "a")
+  quo = input("\nEnter Any Quote: ")
+  f.write(quo)
+  f.close()
 
 if __name__== "__main__":
   primary()
